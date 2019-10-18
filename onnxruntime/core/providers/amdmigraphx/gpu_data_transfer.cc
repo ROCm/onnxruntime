ce78a0dd3d8723058181b95abf34f4c398c3ca44
Shucai Xiao <Shucai.Xiao@amd.com>
2019-10-18 17:04:11 -0500

    more code backup

--- onnxruntime/core/providers/amdmigraphx/gpu_data_transfer.cc
+++ onnxruntime/core/providers/amdmigraphx/gpu_data_transfer.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include "migraphx_inc.h"
 #include "gpu_data_transfer.h"
 
 namespace onnxruntime {
---
+++ onnxruntime/core/providers/amdmigraphx/migraphx_inc.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <migraphx/gpu/hip.hpp>
+#include <migraphx/onnx.hpp>
