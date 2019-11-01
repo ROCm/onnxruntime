a119ec16b823835b53d8ceeb77e5fe30b2ba52c9
Shucai Xiao <Shucai.Xiao@amd.com>
2019-11-01 16:51:38 -0500

    fixed another error related to the MiGraphX provider API

--- include/onnxruntime/core/providers/migraphx/migraphx_provider_factory.h
+++ include/onnxruntime/core/providers/migraphx/migraphx_provider_factory.h
@@ -6,7 +6,7 @@
 extern "C" {
 #endif
 
-ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_MiGraphX, _In_ OrtSessionOptions* options);
+ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_MiGraphX, _In_ OrtSessionOptions* options, int device_id);
 
 #ifdef __cplusplus
 }
