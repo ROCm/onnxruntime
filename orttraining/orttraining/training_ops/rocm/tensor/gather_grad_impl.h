17aa6444f9f85fb974e2d1da3494e8961ece8c38
Jeff Daily <jeff.daily@amd.com>
2020-11-18 21:21:06 +0000

    Perform hipify of latest cuda gather grad.

--- orttraining/orttraining/training_ops/rocm/tensor/gather_grad_impl.h
+++ orttraining/orttraining/training_ops/rocm/tensor/gather_grad_impl.h
@@ -9,17 +9,19 @@
 namespace onnxruntime {
 namespace rocm {
 
-template <typename T, typename Tin>
+// unit for handling indexing and counting of gathered indices
+using GatheredIndexIndex_t = int32_t;
+
+template <typename T, typename TIndex>
 void GatherGradImpl(
     const RocmKernel& rocm_kernel,
-    const T* grad_data,
-    const Tin* indices_data,
-    const int64_t num_indices,
-    const int64_t num_weights,
-    const int64_t stride,
-    T* output_data,
-    const int64_t num_inputs,
-    const int64_t param_itrs);
+    const T* dY_data,
+    const TIndex* dX_indices,
+    const GatheredIndexIndex_t num_gathered_indices,
+    const int64_t gather_dimension_size,
+    const int64_t num_gathered_per_index,
+    const int64_t num_batches,
+    T* dX_data);
 
 }  // namespace rocm
 }  // namespace onnxruntime
