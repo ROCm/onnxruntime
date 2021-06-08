7e5733d71ab9c943a65cf95e42f50c72c6334971
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-08 15:15:34 -0500

    update rocm ep to rocm4.2

--- dockerfiles/scripts/install_rocm_deps.sh
+++ dockerfiles/scripts/install_rocm_deps.sh
@@ -25,36 +25,36 @@ cd ../..
 rm -rf rocm-cmake-rocm-3.8.0
 
 # rccl
-wget --quiet https://github.com/ROCmSoftwarePlatform/rccl/archive/rocm-4.0.0.tar.gz
-tar -xzvf rocm-4.0.0.tar.gz
-rm rocm-4.0.0.tar.gz
-cd rccl-rocm-4.0.0
+wget --quiet https://github.com/ROCmSoftwarePlatform/rccl/archive/rocm-4.2.0.tar.gz
+tar -xzvf rocm-4.2.0.tar.gz
+rm rocm-4.2.0.tar.gz
+cd rccl-rocm-4.2.0
 mkdir build
 cd build
 CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=$prefix ..
 make -j8
 make install
 cd ../..
-rm -rf rccl-rocm-4.0.0
+rm -rf rccl-rocm-4.2.0
 
 #rocrand
-wget --quiet https://github.com/ROCmSoftwarePlatform/rocRAND/archive/rocm-4.0.0.tar.gz
-tar -xzvf rocm-4.0.0.tar.gz
-rm rocm-4.0.0.tar.gz
-cd rocRAND-rocm-4.0.0
+wget --quiet https://github.com/ROCmSoftwarePlatform/rocRAND/archive/rocm-4.2.0.tar.gz
+tar -xzvf rocm-4.2.0.tar.gz
+rm rocm-4.2.0.tar.gz
+cd rocRAND-rocm-4.2.0
 mkdir build
 cd build
 CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=$prefix ..
 make -j8
 make install
 cd ../..
-rm -rf rocRAND-rocm-4.0.0
+rm -rf rocRAND-rocm-4.2.0
 
 #hipcub
-wget --quiet https://github.com/ROCmSoftwarePlatform/hipCUB/archive/rocm-4.0.0.tar.gz
-tar -xzvf rocm-4.0.0.tar.gz
-rm rocm-4.0.0.tar.gz
-cd hipCUB-rocm-4.0.0
+wget --quiet https://github.com/ROCmSoftwarePlatform/hipCUB/archive/rocm-4.2.0.tar.gz
+tar -xzvf rocm-4.2.0.tar.gz
+rm rocm-4.2.0.tar.gz
+cd hipCUB-rocm-4.2.0
 mkdir build
 cd build
 CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=$prefix ..
@@ -62,18 +62,18 @@ make -j8
 make package
 make install
 cd ../..
-rm -rf hipCUB-rocm-4.0.0
+rm -rf hipCUB-rocm-4.2.0
 
 #rocprim
-wget --quiet https://github.com/ROCmSoftwarePlatform/rocPRIM/archive/rocm-4.0.0.tar.gz
-tar -xzvf rocm-4.0.0.tar.gz
-rm rocm-4.0.0.tar.gz
-cd rocPRIM-rocm-4.0.0
+wget --quiet https://github.com/ROCmSoftwarePlatform/rocPRIM/archive/rocm-4.2.0.tar.gz
+tar -xzvf rocm-4.2.0.tar.gz
+rm rocm-4.2.0.tar.gz
+cd rocPRIM-rocm-4.2.0
 mkdir build
 cd build
 CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=$prefix ..
 make -j8
 make install
 cd ../..
-rm -rf rocPRIM-rocm-4.0.0
+rm -rf rocPRIM-rocm-4.2.0
 
