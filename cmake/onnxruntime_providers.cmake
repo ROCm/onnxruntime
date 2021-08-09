3a8acc9afa4ec7d4a2c04fdbc7bcb2c236aed30b
Jeff Daily <jeff.daily@amd.com>
2021-08-09 22:20:03 +0000

    change hip_add_library() to onnxruntime_add_shared_library_module()
    This now builds the rocm provider as a *.so.
    
    Required passing flake8, so fixed amd_hipify.py
    
    Also required addressing HIP APIs that use nodiscard.

--- cmake/onnxruntime_providers.cmake
+++ cmake/onnxruntime_providers.cmake
@@ -1083,7 +1083,7 @@ if (onnxruntime_USE_ROCM)
   # Generate GPU code for GFX9 Generation
   list(APPEND HIP_CLANG_FLAGS --amdgpu-target=gfx906 --amdgpu-target=gfx908)
 
-  hip_add_library(onnxruntime_providers_rocm ${onnxruntime_providers_rocm_src})
+  onnxruntime_add_shared_library_module(onnxruntime_providers_rocm ${onnxruntime_providers_rocm_src})
 
   target_compile_options(onnxruntime_providers_rocm PRIVATE -Wno-sign-compare -D__HIP_PLATFORM_HCC__=1)
   check_cxx_compiler_flag(-Wno-unused-parameter HAS_NO_UNUSED_PARAMETER)
