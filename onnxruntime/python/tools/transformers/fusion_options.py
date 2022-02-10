ecbe38cd08ff4ce87eeec121100f932376565e07
Shucai Xiao <Shucai.Xiao@amd.com>
2022-02-10 13:02:47 -0600

    disable the fusion flag related to fp16, so model can run on migraphx

--- onnxruntime/python/tools/transformers/fusion_options.py
+++ onnxruntime/python/tools/transformers/fusion_options.py
@@ -16,11 +16,16 @@ class FusionOptions:
     """ Options of fusion in graph optimization
     """
     def __init__(self, model_type):
-        self.enable_gelu = True
-        self.enable_layer_norm = True
-        self.enable_attention = True
-        self.enable_skip_layer_norm = True
-        self.enable_embed_layer_norm = True
+#        self.enable_gelu = True
+#        self.enable_layer_norm = True
+#        self.enable_attention = True
+#        self.enable_skip_layer_norm = True
+#        self.enable_embed_layer_norm = True
+        self.enable_gelu = False
+        self.enable_layer_norm = False
+        self.enable_attention = False
+        self.enable_skip_layer_norm = False
+        self.enable_embed_layer_norm = False
         self.enable_bias_skip_layer_norm = True
         self.enable_bias_gelu = True
         self.enable_gelu_approximation = False
@@ -66,19 +71,19 @@ class FusionOptions:
     @staticmethod
     def add_arguments(parser: ArgumentParser):
         parser.add_argument('--disable_attention', required=False, action='store_true', help="disable Attention fusion")
-        parser.set_defaults(disable_attention=False)
+        parser.set_defaults(disable_attention=True)
 
         parser.add_argument('--disable_skip_layer_norm',
                             required=False,
                             action='store_true',
                             help="disable SkipLayerNormalization fusion")
-        parser.set_defaults(disable_skip_layer_norm=False)
+        parser.set_defaults(disable_skip_layer_norm=True)
 
         parser.add_argument('--disable_embed_layer_norm',
                             required=False,
                             action='store_true',
                             help="disable EmbedLayerNormalization fusion")
-        parser.set_defaults(disable_embed_layer_norm=False)
+        parser.set_defaults(disable_embed_layer_norm=True)
 
         parser.add_argument('--disable_bias_skip_layer_norm',
                             required=False,
