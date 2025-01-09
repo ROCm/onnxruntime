6f119cb24c0fdff76bde28c43a3a84cc09f5d4f6
Uros Petkovic <urpetkov@amd.com>
2025-01-09 15:39:27 -0600

    Reordering ROCm and MIGraphX elifs

--- onnxruntime/python/onnxruntime_pybind_ortvalue.cc
+++ onnxruntime/python/onnxruntime_pybind_ortvalue.cc
@@ -177,13 +177,13 @@ void addOrtValueMethods(pybind11::module& m) {
             throw std::runtime_error("The provided device id doesn't match any available GPUs on the machine.");
           }
           allocator = GetCudaAllocator(device.Id());
-#elif USE_MIGRAPHX
-          allocator = GetMIGraphXAllocator(device.Id());
 #elif USE_ROCM
           if (!IsRocmDeviceIdValid(logging::LoggingManager::DefaultLogger(), device.Id())) {
             throw std::runtime_error("The provided device id doesn't match any available GPUs on the machine.");
           }
           allocator = GetRocmAllocator(device.Id());
+#elif USE_MIGRAPHX
+          allocator = GetMIGraphXAllocator(device.Id());
 #else
       throw std::runtime_error(
           "Can't allocate memory on the CUDA device using this package of OnnxRuntime. "
