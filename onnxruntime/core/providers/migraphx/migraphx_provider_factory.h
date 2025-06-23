b1a0d050a1453d3e3d864b4fb8e8a2e934991fe0
Artur Wojcik <artur.wojcik@amd.com>
2025-06-23 22:44:23 +0200

    Change to OrtDevice::DeviceId

--- onnxruntime/core/providers/migraphx/migraphx_provider_factory.h
+++ onnxruntime/core/providers/migraphx/migraphx_provider_factory.h
@@ -8,11 +8,11 @@ namespace onnxruntime {
 class IAllocator;
 
 struct ProviderInfo_MIGraphX {
-  virtual std::unique_ptr<IAllocator> CreateMIGraphXAllocator(int16_t device_id, const char* name) = 0;
-  virtual std::unique_ptr<IAllocator> CreateMIGraphXPinnedAllocator(int16_t device_id, const char* name) = 0;
+  virtual std::unique_ptr<IAllocator> CreateMIGraphXAllocator(OrtDevice::DeviceId device_id, const char* name) = 0;
+  virtual std::unique_ptr<IAllocator> CreateMIGraphXPinnedAllocator(OrtDevice::DeviceId device_id, const char* name) = 0;
   virtual void MIGraphXMemcpy_HostToDevice(void* dst, const void* src, size_t count) = 0;
   virtual void MIGraphXMemcpy_DeviceToHost(void* dst, const void* src, size_t count) = 0;
-  virtual std::shared_ptr<IAllocator> CreateMIGraphXAllocator(int16_t device_id, size_t mem_limit,
+  virtual std::shared_ptr<IAllocator> CreateMIGraphXAllocator(OrtDevice::DeviceId device_id, size_t mem_limit,
     ArenaExtendStrategy arena_extend_strategy, void* alloc_fn, void* free_fn, void* empty_cache_fn, const OrtArenaCfg* default_memory_arena_cfg) = 0;
 
  protected:
