2d57f237c54fb5a811e97b549286a4decbeb4e27
Miodrag Pantic <Miodrag.Pantic@amd.com>
2025-06-27 16:49:42 +0200

    GroupQueryAttention operator moved to CPU for execution since it is not supported for RDNA architectures.

--- onnxruntime/core/providers/rocm/rocm_execution_provider.cc
+++ onnxruntime/core/providers/rocm/rocm_execution_provider.cc
@@ -2465,6 +2465,17 @@ ROCMExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph,
       continue;
     }
 
+    auto& device_prop = GetDeviceProp();
+    if (std::string_view(node.Name()).find("GroupQueryAttention") != std::string_view::npos &&
+        std::string_view(device_prop.gcnArchName).find("gfx90a") == std::string_view::npos &&
+        std::string_view(device_prop.gcnArchName).find("gfx942") == std::string_view::npos){
+      LOGS(logger, WARNING)
+        << "Node " << node.Name() << " moved to CPU execution, not supported on "
+        << device_prop.gcnArchName << " arch.";
+      continue;
+    }
+
+
     bool not_supported = false;
     bool force_inside = false;  // for some compute heavy ops, we'll force it to run inside ROCM
     if ("LSTM" == node.OpType() ||
