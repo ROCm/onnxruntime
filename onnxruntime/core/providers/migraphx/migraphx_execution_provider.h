47381955fa621bdc505bf42cf014e636e83bd688
Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
2025-10-02 21:09:01 -0400

    Fix for full graph being set to getSubGraph with dangling outputs in graph nodes (#179)

--- onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
+++ onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
@@ -84,7 +84,7 @@ class MIGraphXExecutionProvider : public IExecutionProvider {
   static AllocatorPtr CreateMIGraphXAllocator(OrtDevice::DeviceId device_id, size_t migx_mem_limit, ArenaExtendStrategy arena_extend_strategy,
                                               MIGraphXExecutionProviderExternalAllocatorInfo external_alloc_info, const OrtArenaCfg* arena_cfg);
 
-  std::unique_ptr<IndexedSubGraph> GetSubGraph(const std::vector<std::size_t>& graph_nodes_index, const GraphViewer& graph) const;
+  std::unique_ptr<IndexedSubGraph> GetSubGraph(const std::vector<std::size_t>& graph_nodes_index, const GraphViewer& graph, bool is_graph_split) const;
   void RegisterStreamHandlers(IStreamCommandHandleRegistry& stream_handle_registry, AllocatorMap& allocators) const override;
   OrtDevice GetOrtDeviceByMemType(OrtMemType mem_type) const override;
   std::vector<AllocatorPtr> CreatePreferredAllocators() override;
